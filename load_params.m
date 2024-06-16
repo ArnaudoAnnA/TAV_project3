f912c1db8257a33cd8986713a858bdb57fc2cb6d
angeladifazio <angeladifazio00@gmail.com>
2024-06-16 14:45:04 +0200

    code reorganization

--- load_params.m
+++ load_params.m
@@ -6,20 +6,19 @@ lf = 1.3;
 a = lf;
 lr = 1.5;
 b = lr;
-Vxb=80/3.6; %m/s
 Cf = 2*60e3;
 %Cr = 2*57e3;% under
-%Cr = 57e3; % over
-Cr = Cf*a/b; % neutral
+Cr = 57e3; % over
+%Cr = Cf*a/b; % neutral
 l = lf+lr;
 CR=Cr;
 CF=Cf;
 L=l;
 tau_s=1;
-Vv=Vxb;
 t_end_sim=200;
 
 
 delta_max = 25;
 
-Vxb = 80/3.6; % [m/s]
+Vxb = 60/3.6; % [m/s]
+Vv=Vxb;
